b1836b05b573d9f69ca1de21c7caa0e1d6ad09f5
Ivan Ivanov <ivan.ivanov@app-streams.com>
2023-01-14 12:16:52 +0200

    add comments from docs

--- src/app/slices/usersSlice.js
+++ src/app/slices/usersSlice.js
@@ -5,11 +5,41 @@ const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
 const initialState = []
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+// createAsyncThunk  https://redux-toolkit.js.org/api/createAsyncThunk
+/**
+ * A function that accepts a Redux action type string and a callback function that should return a promise. It generates promise lifecycle action types based on the action type prefix that you pass in, and returns a thunk action creator that will run the promise callback and dispatch the lifecycle actions based on the returned promise.
+
+This abstracts the standard recommended approach for handling async request lifecycles.
+
+It does not generate any reducer functions, since it does not know what data you're fetching, how you want to track loading state, or how the data you return needs to be processed. You should write your own reducer logic that handles these actions, with whatever loading state and processing logic is appropriate for your own app.
+
+ */
+/**
+ * createAsyncThunk accepts three parameters: a string action type value, a payloadCreator callback, and an options object
+ * type
+A string that will be used to generate additional Redux action type constants, representing the lifecycle of an async request:
+
+For example, a type argument of 'users/requestStatus' will generate these action types:
+
+    pending: 'posts/addNewPost/pending'
+    fulfilled: 'posts/addNewPost/fulfilled'
+    rejected: 'posts/addNewPost/rejected'
+ */
+export const fetchUsers = createAsyncThunk('users/fetchUsers',
+
+/**
+ *  {https://redux-toolkit.js.org/api/createAsyncThunk#payloadcreator} 
+ * A callback function that should return a promise containing the result of some asynchronous logic. It may also return a value synchronously. If there is an error, it should either return a rejected promise containing an Error instance or a plain value such as a descriptive error message or otherwise a resolved promise with a RejectWithValue argument as returned by the thunkAPI.rejectWithValue function.
+ */
+async () => {
     const response = await axios.get(USERS_URL);
     return response.data
 })
 
+//https://redux-toolkit.js.org/usage/usage-guide#simplifying-slices-with-createslice
+/**
+ * Redux Toolkit includes a createSlice function that will auto-generate the action types and action creators for you, based on the names of the reducer functions you provide.
+ */
 const usersSlice = createSlice({
     name: 'users',
     initialState,
--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from './slices/postsSlice';
 import usersReducer from './slices/usersSlice';
 
-
+//configureStore function automatically sets up the thunk middleware by default, so you can immediately start writing thunks as part of your application code
 export const store = configureStore({
     reducer: {
         posts: postsReducer,
